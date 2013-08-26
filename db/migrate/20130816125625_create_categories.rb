16519fe83bb60fe7e28462e7a39abeae5fe7f8c1
Jeff Higham <jeffhigham@gmail.com>
2013-08-26 14:42:54 -0600

    fixed database column for message

--- db/migrate/20130816125625_create_categories.rb
+++ db/migrate/20130816125625_create_categories.rb
@@ -3,7 +3,7 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.string :name
       t.string :title
-      t.string :message
+      t.text :message
 
       t.timestamps
     end
